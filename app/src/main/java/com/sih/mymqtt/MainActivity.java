216cd1adb3b8bd32a7b76b2701498181bbeac08c
rascaldom <rascaldom@gmail.com>
2017-04-04 17:50:03 +0900

    안드로이드 mqtt 테스트 코드

--- app/src/main/java/com/sih/mymqtt/MainActivity.java
+++ app/src/main/java/com/sih/mymqtt/MainActivity.java
@@ -1,45 +1,161 @@
 package com.sih.mymqtt;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements MqttCallback {
+
+    private HistoryAdapter mAdapter;
+
+    private MqttAndroidClient mqttAndroidClient;
+
+    private final String serverUri = "tcp://ssadata.iptime.org:1883";
+    private String clientId = "sih";
+    private final String subscriptionTopic = "test";
+    private final String publishTopic = "test";
+    private final String publishMessage = "Hello SKCOMMS!";
+
+    private EditText etMessage;
+    private Button btSend;
+
+    private InputMethodManager imm;
+
+    private boolean isSend = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publishMessage(publishMessage);
+            }
+        });
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.history_recycler_view);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new HistoryAdapter(new ArrayList<String>());
+        mRecyclerView.setAdapter(mAdapter);
+
+        imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+
+        etMessage = (EditText) findViewById(R.id.et_publish);
+        btSend = (Button) findViewById(R.id.bt_send);
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etMessage != null && !"".equals(etMessage.getText().toString())) {
+                    publishMessage(etMessage.getText().toString());
+                    imm.hideSoftInputFromWindow(etMessage.getWindowToken(), 0);
+                    etMessage.setText("");
+                }
+            }
+        });
+
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+        mqttAndroidClient.setCallback(this);
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setCleanSession(false);
+
         try {
-            MqttAndroidClient mqttClient = new MqttAndroidClient(this, "ssadata.iptime.org:1883", "sih");
-            mqttClient.connect();
-            mqttClient.subscribe("test", 0);
-            mqttClient.setCallback(new MqttCallback() {
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+
                 @Override
-                public void connectionLost(Throwable cause) {
-                    Log.d(this.getClass().getSimpleName(), "[rascaldom] connectionLost()");
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    subscribeToTopic();
                 }
 
                 @Override
-                public void messageArrived(String topic, MqttMessage message) throws Exception {
-                    Log.d(this.getClass().getSimpleName(), "[rascaldom] messageArrived()");
-                }
-
-                @Override
-                public void deliveryComplete(IMqttDeliveryToken token) {
-                    Log.d(this.getClass().getSimpleName(), "[rascaldom] deliveryComplete()");
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    addToHistory("Failed to connect to: " + serverUri);
                 }
             });
         } catch (MqttException e) {
             e.printStackTrace();
         }
+    }
+
+    private void addToHistory(String mainText) {
+        System.out.println("LOG: " + mainText);
+        mAdapter.add(mainText);
+        Snackbar.make(findViewById(android.R.id.content), mainText, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+    public void subscribeToTopic() {
+        try {
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, null, new IMqttActionListener() {
+
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    addToHistory("Subscribed!");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    addToHistory("Failed to subscribe");
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void publishMessage(String msg) {
+        try {
+            MqttMessage message = new MqttMessage(msg.getBytes());
+            mqttAndroidClient.publish(publishTopic, message);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void connectionLost(Throwable cause) {
+        addToHistory("The Connection was lost.");
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+        String who = isSend ? "Me: " : "You: ";
+        addToHistory(who + new String(message.getPayload()));
+        isSend = false;
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken token) {
+        isSend = true;
     }
 }
